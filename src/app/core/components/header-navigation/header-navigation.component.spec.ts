f79bd76545654f670cd44769ff3d8abe02627983
Alexander Blank <>
2024-05-21 19:31:18 +0200

    feat: added header navigation

---
+++ src/app/core/components/header-navigation/header-navigation.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderNavigationComponent } from './header-navigation.component';
+
+describe('HeaderNavigationComponent', () => {
+  let component: HeaderNavigationComponent;
+  let fixture: ComponentFixture<HeaderNavigationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HeaderNavigationComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(HeaderNavigationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
