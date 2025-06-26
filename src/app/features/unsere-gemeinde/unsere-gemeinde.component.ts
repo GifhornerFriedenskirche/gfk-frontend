da00610f56af8f21210856fe5ea2c05e3e7cf576
Alexander Blank <>
2025-06-26 10:18:54 +0200

    feat: code refactoring page slugs

--- src/app/features/unsere-gemeinde/unsere-gemeinde.component.ts
+++ src/app/features/unsere-gemeinde/unsere-gemeinde.component.ts
@@ -5,6 +5,7 @@ import { HttpErrorResponse } from '@angular/common/http'; // Added import
 import { Hero } from '../../core/interfaces/hero.interface';
 import { PageDataService } from '../../core/services/page-data.service';
 import { BasePageComponent } from '../../shared/components/base-page.component';
+import { PAGE_SLUGS } from '../../core/constants/app.constants';
 
 @Component({
   selector: 'app-unsere-gemeinde',
@@ -25,11 +26,10 @@ export class UnsereGemeindeComponent
     // to prevent ExpressionChangedAfterItHasBeenCheckedError.
     setTimeout(() => this.loadData(), 0);
   }
-
   loadData(): void {
     this.resetErrorState();
-    this.pageDataService.getPageData('unsere-gemeinde').subscribe({
-      // Changed to getPageData with slug
+    this.pageDataService.getPageData(PAGE_SLUGS.COMMUNITY).subscribe({
+      // Using the constant PAGE_SLUGS.COMMUNITY instead of hardcoded string
       next: (heroData: Hero) => {
         this.data = heroData;
       },
